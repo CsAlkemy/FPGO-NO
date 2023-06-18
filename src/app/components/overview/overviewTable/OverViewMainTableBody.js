9312ddc38aeb62c12b0436b7a06c25b5e962def7
zahid-fp <zahedul.islam@frontpayment.no>
2023-06-18 11:51:57 +0600

    refund btn alignment

--- src/app/components/overview/overviewTable/OverViewMainTableBody.js
+++ src/app/components/overview/overviewTable/OverViewMainTableBody.js
@@ -1876,7 +1876,7 @@ export default function OverViewMainTableBody(props) {
             ""
           ) : props.row.status.toLowerCase() === "completed" &&
             refundableAmount > 0 ? (
-            <TableCell key={`${props.row.uuid}-${rdt}`} align="right">
+            <TableCell key={`${props.row.uuid}-${rdt}`} align="center">
               <CustomTooltip
                 disableFocusListener
                 title={t("label:refundFromReservations")}
